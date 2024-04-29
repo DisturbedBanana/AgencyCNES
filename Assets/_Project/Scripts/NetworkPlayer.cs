42b0c0bf485287aba4e49c74b5f237ea17b1c3a7
DisturbedBanana <tompouts@gmail.com>
2024-04-29 16:27:00 +0200

    [ADD] multiplayer base

---
+++ Assets/_Project/Scripts/NetworkPlayer.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class NetworkPlayer : NetworkBehaviour
+{
+    public Transform root;
+    public Transform head;
+    public Transform leftHand;
+    public Transform rightHand;
+
+    public Renderer[] meshesToDisable;
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        if (IsOwner)
+        {
+            foreach (var item in meshesToDisable)
+            {
+                item.enabled = false;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (IsOwner)
+        {
+            root.position = VrRigReferences.Singleton.root.position;
+            root.rotation = VrRigReferences.Singleton.root.rotation;
+
+            head.position = VrRigReferences.Singleton.head.position;
+            head.rotation = VrRigReferences.Singleton.head.rotation;
+
+            leftHand.position = VrRigReferences.Singleton.leftHand.position;
+            leftHand.rotation = VrRigReferences.Singleton.leftHand.rotation;
+
+            rightHand.position = VrRigReferences.Singleton.rightHand.position;
+            rightHand.rotation = VrRigReferences.Singleton.rightHand.rotation;
+        }
+    }
+}
