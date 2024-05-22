f83c6ee2602f6a412351733d56489dfa5adc32c0
Nicolas Kohler <nicolas.kohler@free.fr>
2024-05-22 11:17:31 +0200

    [ADD] Limit player movement

---
+++ Assets/_Project/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Utilities;
+
+public class PlayerController : CharacterControllerDriver
+{
+    private Rigidbody _rigidbody;
+    [SerializeField] private float _maxMagnitude;
+    [SerializeField] private float _magnitude;
+
+    // Start is called before the first frame update
+    protected new void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_rigidbody == null)
+            return;
+
+        _magnitude = _rigidbody.velocity.magnitude;
+
+        if (_rigidbody.velocity.magnitude > _maxMagnitude)
+        {
+            Debug.Log("Too fast");
+            _rigidbody.velocity -= _rigidbody.velocity * Time.deltaTime;
+        }
+        
+    }
+}
