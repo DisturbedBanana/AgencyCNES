a086402acca09bd40d0355126cc98ec05b54ebd5
Nicolas Kohler <nicolas.kohler@free.fr>
2024-05-22 13:38:55 +0200

    [MOVE] Move and clean folders

--- Assets/Proxima/Runtime/ProximaServer.cs
+++
@@ -1,9 +0,0 @@
-namespace Proxima
-{
-    internal interface ProximaServer
-    {
-        void Start(string displayName, string password);
-        void Stop();
-        bool TryGetMessage(out (ProximaConnection, string) message);
-    }
-}
