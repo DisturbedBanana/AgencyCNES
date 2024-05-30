e9c573a2ad5fe94aa245e60f670bc54d4005e83c
DisturbedBanana <tompouts@gmail.com>
2024-05-30 09:11:24 +0200

    [EDIT] Valve logic and checks for validation

--- Assets/_Project/Scripts/ValveManager.cs
+++ Assets/_Project/Scripts/ValveManager.cs
@@ -36,6 +36,7 @@ public class ValveManager : NetworkBehaviour
         }
 
         _isSolved = true;
+        GameState.instance.ChangeState(GameState.GAMESTATES.LAUNCH);
         Debug.LogError("Valve puzzle solved!");
     }
 }
