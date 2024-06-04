530d751a9c3a61a08ecd58e22f186127f3b28586
Nicolas Kohler <nicolas.kohler@free.fr>
2024-06-04 16:53:52 +0200

    [ADD] Frequency solo mission control

--- Assets/_Project/Scripts/Enigmes/Frequency.cs
+++ Assets/_Project/Scripts/Enigmes/Frequency.cs
@@ -1,35 +1,93 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 public class Frequency : MonoBehaviour
 {
-    public LineRenderer myLineRenderer;
-    public int points;
-    public float amplitude = 1;
-    public float frequency = 1;
-    public Vector2 xLimits = new Vector2(0, 1);
-    public float movementSpeed = 1;
-    [Range(0, 2 * Mathf.PI)]
-    public float radians;
-    void Start()
+    [Header("Line renderer")]
+    [SerializeField] private LineRenderer _myLineRenderer;
+    [SerializeField] private float _lineWidth;
+
+    [SerializeField] private int _pointsMultiplicator;
+    [SerializeField] private float _sensitivity;
+
+    [Header("Amplitude")]
+    [SerializeField] private float _amplitude;
+    [SerializeField] private float _maxAmplitude;
+    [SerializeField] private float _minAmplitude;
+
+    [Header("Frequence")]
+    [SerializeField] private float _frequency;
+    [SerializeField] private float _maxFrequency;
+    [SerializeField] private float _minFrequency;
+
+    [SerializeField] private Vector2 _xLimits;
+    [SerializeField] private float _movementSpeed;
+    [SerializeField, Range(0, 2 * Mathf.PI)] private float _radians;
+
+    private void Reset()
     {
-        myLineRenderer = GetComponent<LineRenderer>();
+        _myLineRenderer = GetComponent<LineRenderer>();
+        _lineWidth = 0.1f;
+        _myLineRenderer.startWidth = _myLineRenderer.endWidth = _lineWidth;
+        _pointsMultiplicator = 150;
+
+        _amplitude = _maxAmplitude = 1f;
+        _frequency = _maxFrequency = 1f;
+        _xLimits = new Vector2(0, 1);
+        _movementSpeed = 1;
+        _sensitivity = 0.01f;
     }
+
+    private void Start()
+    {
+        _myLineRenderer.startWidth = _myLineRenderer.endWidth = _lineWidth;
+    }
+
+    public void ChangeAmplitude(float value)
+    {
+        if (value > _amplitude && (_amplitude + GetSensitivity()) < _maxAmplitude)
+        {
+            _amplitude += GetSensitivity(); 
+        }
+        else if (value < _amplitude && (_amplitude - GetSensitivity()) > _minAmplitude)
+        {
+            _amplitude -= GetSensitivity();
+        }
+    }
+
+    public void ChangeFrequency(float value)
+    {
+        if (value > _frequency && (_frequency + GetSensitivity()) < _maxFrequency)
+        {
+            _frequency += GetSensitivity();
+        }
+        else if (value < _frequency && (_frequency - GetSensitivity()) > _minFrequency)
+        {
+            _frequency -= GetSensitivity();
+        }
+    }
+
+    private float GetSensitivity() => _sensitivity / 100;
+
 
     void Draw()
     {
-        float xStart = xLimits.x;
+        float xStart = _xLimits.x;
         float Tau = 2 * Mathf.PI;
-        float xFinish = xLimits.y;
+        float xFinish = _xLimits.y;
 
-        myLineRenderer.positionCount = points;
-        for (int currentPoint = 0; currentPoint < points; currentPoint++)
+        _myLineRenderer.positionCount = _pointsMultiplicator;
+        for (int currentPoint = 0; currentPoint < _myLineRenderer.positionCount; currentPoint++)
         {
-            float progress = (float)currentPoint / (points - 1);
+            float progress = (float)currentPoint / (_myLineRenderer.positionCount - 1);
             float x = Mathf.Lerp(xStart, xFinish, progress);
-            float y = amplitude * Mathf.Sin((Tau * frequency * x) + (Time.timeSinceLevelLoad * movementSpeed));
-            myLineRenderer.SetPosition(currentPoint, new Vector3(x, y, 0));
+            float y = _amplitude * Mathf.Sin((Tau * _frequency * x) + (Time.timeSinceLevelLoad * _movementSpeed));
+            Vector3 newBeginPos = transform.localToWorldMatrix * new Vector4(x, y, 0, 1);
+
+            _myLineRenderer.SetPosition(currentPoint, newBeginPos);
+            
         }
     }
 
